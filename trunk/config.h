5d10cfc703b7e329e37796762d63cbf51d8ef094
driedfruit <driedfruit@5e3da121-8431-40aa-8d6e-0350d27d023d>
2008-09-14 01:00:49 +0000

    Slightly better dirs (+memleak?), accounts are friendlier, so are files.

--- trunk/config.h
+++ trunk/config.h
@@ -1,4 +1,1 @@
-/* config.h.  Generated from config.h.in by configure.  */
-//#define CUSTOM_PLUGIN_PATH     "/home/driedfruit/soft/purplebot/plugins"
-//#define PLUGIN_SAVE_PREF       "/home/driedfruit/soft/purplebot/saved"
-#define LOG_FILE					 "/home/driedfruit/soft/purplebot/purpled.log"
+
